e734175deef9e70fe52ae9b147303e770d6cea8a
pjavadi84 <pouya.javadi.resume@gmail.com>
2020-03-07 13:25:23 -0800

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,22 @@
+class StudentsController < ApplicationController
+    def Create 
+
+    end
+
+    def new
+
+    end
+
+    def show
+
+    end
+
+    def edit 
+
+    end
+
+    def update
+        
+    end
+
+end

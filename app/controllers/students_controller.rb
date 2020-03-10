94f2955e9d32529deaf5881700051e9624f0f285
allisoncortez <allizetorc@gmail.com>
2020-03-10 16:31:08 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,7 @@
 class StudentsController < ApplicationController
 
     def show
+        @student = Student.find(params[:id])
     end
 
     def new
@@ -8,6 +9,7 @@ class StudentsController < ApplicationController
     end
 
     def edit
+        @student = Student.find(params[:id])
     end
 
     def create
